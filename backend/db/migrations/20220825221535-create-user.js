5b9b65738d3167f3f6164f13b7915e90cf8d7b3a
DevSPK <seankennedy75@gmail.com>
2022-08-26 11:30:53 -0700

    added firstName and lastName attributes to User model and migration

--- backend/db/migrations/20220825221535-create-user.js
+++ backend/db/migrations/20220825221535-create-user.js
@@ -22,6 +22,14 @@ module.exports = {
 				type: Sequelize.STRING.BINARY,
 				allowNull: false
 			},
+			fistName: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			lastName: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
 			createdAt: {
 				allowNull: false,
 				type: Sequelize.DATE,
