b7a9c037b50039e2c56acfea6209ee67b52c7ffa
DevSPK <seankennedy75@gmail.com>
2022-08-26 11:51:27 -0700

    added firstName and lastName fields to the demo-user seeder

--- backend/db/seeders/20220825222409-demo-user.js
+++ backend/db/seeders/20220825222409-demo-user.js
@@ -21,17 +21,23 @@ module.exports = {
 				{
 					email: "demo@user.io",
 					username: "Demo-lition",
-					hashedPassword: bcrypt.hashSync("password")
+					hashedPassword: bcrypt.hashSync("password"),
+					firstName: "Fred",
+					lastName: "Flintstone"
 				},
 				{
 					email: "user1@user.io",
 					username: "FakeUser1",
-					hashedPassword: bcrypt.hashSync("password2")
+					hashedPassword: bcrypt.hashSync("password2"),
+					firstName: "Wilma",
+					lastName: "Flintstone"
 				},
 				{
 					email: "user2@user.io",
 					username: "FakeUser2",
-					hashedPassword: bcrypt.hashSync("password3")
+					hashedPassword: bcrypt.hashSync("password3"),
+					firstName: "Barney",
+					lastName: "Rubble"
 				}
 			],
 			{}
