286e8f795ea38909dd96c9815e234c001683edc0
DevSPK <seankennedy75@gmail.com>
2022-08-29 12:36:15 -0700

    created EventImage model and migration

--- backend/db-commands.sh
+++ backend/db-commands.sh
@@ -21,5 +21,10 @@ npx sequelize-cli model:generate --name GroupImage --attributes groupId:integer,
 
 npx sequelize-cli model:generate --name Event --attributes venueId:integer,groupId:integer,name:string,description:text,type:enum,capacity:integer,price:integer,startDate:date,endDate:date
 
+# sixth EventImages model and migrations
+
+npx sequelize-cli model:generate --name EventImage --attributes eventId:integer,url:string,preview:boolean
+
+
 # last
 npx sequelize-cli model:generate --name Attendance --attributes eventId:integer,userId:integer,status:enum
