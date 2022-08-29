c6de8c5089162476287ce8f171ed6a72e65298c1
DevSPK <seankennedy75@gmail.com>
2022-08-28 18:20:40 -0700

    added associations from User to Group

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -50,6 +50,7 @@ module.exports = (sequelize, DataTypes) => {
 		 */
 		static associate(models) {
 			// define association here
+			User.hasMany(models.Group, { foreignKey: "organizerId" });
 		}
 	}
 	User.init(
