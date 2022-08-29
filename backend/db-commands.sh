3dbf51ce35a872f0e6471d5ef725a8338cb603cc
DevSPK <seankennedy75@gmail.com>
2022-08-29 10:08:00 -0700

    created model and migration files for Venues table

--- backend/db-commands.sh
+++ backend/db-commands.sh
@@ -3,5 +3,10 @@
 npx sequelize-cli model:generate --name Group --attributes organizerId:integer,name:string,about:text,type:enum,private:boolean,city:string,state:string
 
 
+# second Venue model and migrations
+
+npx sequelize-cli model:generate --name Venue --attributes groupId:integer,address:string,city:string,state:string,lat:decimal,lng:decimal
+
+
 # last
 npx sequelize-cli model:generate --name Attendance --attributes eventId:integer,userId:integer,status:enum
