ccaae4ee96ea0f9ad100bec38d1fecac210a35aa
DevSPK <seankennedy75@gmail.com>
2022-08-29 11:23:26 -0700

    created model and migration for Membership

--- backend/db-commands.sh
+++ backend/db-commands.sh
@@ -8,5 +8,9 @@ npx sequelize-cli model:generate --name Group --attributes organizerId:integer,n
 npx sequelize-cli model:generate --name Venue --attributes groupId:integer,address:string,city:string,state:string,lat:decimal,lng:decimal
 
 
+# third Membership model and migrations
+
+npx sequelize-cli model:generate --name Membership --attributes userId:integer,groupId:integer,status:enum
+
 # last
 npx sequelize-cli model:generate --name Attendance --attributes eventId:integer,userId:integer,status:enum
