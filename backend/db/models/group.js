a0847536d169983b012caf3fe69f33a8c1da37c5
DevSPK <seankennedy75@gmail.com>
2022-08-28 18:54:44 -0700

    added validations and constraints to Groups model and migration, successfully ran groups migration

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -15,12 +15,37 @@ module.exports = (sequelize, DataTypes) => {
 	Group.init(
 		{
 			organizerId: DataTypes.INTEGER,
-			name: DataTypes.STRING,
-			about: DataTypes.TEXT,
-			type: DataTypes.ENUM,
-			private: DataTypes.BOOLEAN,
-			city: DataTypes.STRING,
-			state: DataTypes.STRING
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					len: [0, 60]
+				}
+			},
+			about: {
+				type: DataTypes.TEXT,
+				allowNull: false,
+				validate: {
+					len: [50, 1000]
+				}
+			},
+			type: {
+				type: DataTypes.ENUM,
+				allowNull: false
+			},
+			private: {
+				type: DataTypes.BOOLEAN,
+				allowNull: false,
+				default: true
+			},
+			city: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			state: {
+				type: DataTypes.STRING,
+				allowNull: false
+			}
 		},
 		{
 			sequelize,
