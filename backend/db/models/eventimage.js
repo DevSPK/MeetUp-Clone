952ac912636ac82c9681541f1f20dcc532fa886a
DevSPK <seankennedy75@gmail.com>
2022-08-29 12:40:31 -0700

    added validators, constraints, and association between eventimage and events

--- backend/db/models/eventimage.js
+++ backend/db/models/eventimage.js
@@ -1,25 +1,31 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class EventImage extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  EventImage.init({
-    eventId: DataTypes.INTEGER,
-    url: DataTypes.STRING,
-    preview: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'EventImage',
-  });
-  return EventImage;
+	class EventImage extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			EventImage.belongsTo(models.Event, { foreignKey: "eventId" });
+		}
+	}
+	EventImage.init(
+		{
+			eventId: DataTypes.INTEGER,
+			url: DataTypes.STRING,
+			preview: {
+				type: DataTypes.BOOLEAN,
+				allowNull: false,
+				default: false
+			}
+		},
+		{
+			sequelize,
+			modelName: "EventImage"
+		}
+	);
+	return EventImage;
 };
