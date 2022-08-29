0aaadbc8c0cdd8ac70ab97ef757b2685e55650b1
DevSPK <seankennedy75@gmail.com>
2022-08-29 10:26:42 -0700

    added associations between Groups and Venues

--- backend/db/migrations/20220829170624-create-venue.js
+++ backend/db/migrations/20220829170624-create-venue.js
@@ -1,42 +1,50 @@
-'use strict';
+"use strict";
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Venues', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      groupId: {
-        type: Sequelize.INTEGER
-      },
-      address: {
-        type: Sequelize.STRING
-      },
-      city: {
-        type: Sequelize.STRING
-      },
-      state: {
-        type: Sequelize.STRING
-      },
-      lat: {
-        type: Sequelize.DECIMAL
-      },
-      lng: {
-        type: Sequelize.DECIMAL
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Venues');
-  }
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("Venues", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			groupId: {
+				type: Sequelize.INTEGER,
+				references: { model: "Groups" }
+			},
+			address: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			city: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			state: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			lat: {
+				type: Sequelize.DECIMAL,
+				allowNull: false
+			},
+			lng: {
+				type: Sequelize.DECIMAL,
+				allowNull: false
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+			}
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("Venues");
+	}
 };
--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
 		static associate(models) {
 			// define association here
 			Group.belongsTo(models.User, { foreignKey: "organizerId" });
+			Group.hasMany(models.Venue, { foreignKey: "groupId" });
 		}
 	}
 	Group.init(
