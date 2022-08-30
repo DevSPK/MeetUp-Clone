464ec60fc980723ca42281a45f99212d9c577743
DevSPK <seankennedy75@gmail.com>
2022-08-30 09:46:01 -0700

    successfully counts numMembers locally

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
-const { User, Group } = require("../../db/models");
+const { User, Group, sequelize, Membership } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
+const membership = require("../../db/models/membership");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-	const groups = await Group.findAll();
+	const groups = await Group.findAll({
+		attributes: {
+			include: [
+				[sequelize.fn("COUNT", sequelize.col("Memberships.id")), "numMembers"]
+			]
+		},
+		include: [
+			{
+				model: Membership,
+				where: { status: ["member", "co-host"] },
+				attributes: []
+			}
+		],
+		group: ["Group.id"]
+	});
 	res.json(groups);
 });
 
