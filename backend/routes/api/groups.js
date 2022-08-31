ad257a1eeb63539aa422e25b3cee5c3147c44f72
DevSPK <seankennedy75@gmail.com>
2022-08-30 17:34:28 -0700

    created custom validator for groups

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -17,6 +17,24 @@ router.get("/current", requireAuth, async (req, res, next) => {
 	res.send("success");
 });
 
+const validateGroup = [
+	check("name")
+		.exists({ checkFalsy: true })
+		.isLength({ max: 60 })
+		.withMessage("Name must be 60 characters or less"),
+	check("about")
+		.exists({ checkFalsy: true })
+		.isLength({ min: 50 })
+		.withMessage("About must be 50 characters or more"),
+	check("type")
+		.isIn(["Online", "In person"])
+		.withMessage("Type must be 'Online' or 'In person'"),
+	check("private").isBoolean().withMessage("Private must be a boolean"),
+	check("city").exists({ checkFalsy: true }).withMessage("City is required"),
+	check("state").exists({ checkFalsy: true }).withMessage("State is required"),
+	handleValidationErrors
+];
+
 router.post("/", requireAuth, async (req, res, next) => {
 	const userId = req.user.id;
 
