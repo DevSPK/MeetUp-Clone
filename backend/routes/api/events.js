a7687d6f47a18951f96ac542497f41116648fe54
DevSPK <seankennedy75@gmail.com>
2022-09-01 11:54:12 -0700

    added event router

---
+++ backend/routes/api/events.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const {
+	setTokenCookie,
+	requireAuth,
+	restoreUser
+} = require("../../utils/auth");
+const {
+	User,
+	Group,
+	sequelize,
+	Membership,
+	GroupImage,
+	Venue,
+	Sequelize
+} = require("../../db/models");
+const { check, validationResult } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+const membership = require("../../db/models/membership");
+
+const router = express.Router();
+
+router.post("/:eventId/images", requireAuth, async (req, res, next) => {
+	res.send("success");
+});
+
+module.exports = router;
