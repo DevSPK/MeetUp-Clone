3d35e6b7687e66f3c6c76cb0191db8feb9c26385
DevSPK <seankennedy75@gmail.com>
2022-09-01 16:53:04 -0700

    successfully tested locally

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -40,7 +40,7 @@ router.get("/:eventId", async (req, res) => {
 			model: Venue,
 			attributes: ["id", "groupId", "address", "city", "state", "lat", "lng"]
 		},
-		group: ["Event.id", "EventImage.id", "Group.id", "Venue.id"]
+		group: ["Event.id", "Venue.id"]
 	});
 
 	if (!event) {
