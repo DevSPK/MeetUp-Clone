65b10779027a798d1a9d388d2846514f99511436
DevSPK <seankennedy75@gmail.com>
2022-09-02 18:01:50 -0700

    created routers for group-images and event-images

---
+++ backend/routes/api/event-images.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const {
+	setTokenCookie,
+	requireAuth,
+	restoreUser
+} = require("../../utils/auth");
+const {
+	User,
+	Group,
+	sequelize,
+	Membership,
+	GroupImage,
+	Venue,
+	Event,
+	Attendance,
+	EventImage,
+	Sequelize
+} = require("../../db/models");
+const { check, validationResult } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+const membership = require("../../db/models/membership");
+const user = require("../../db/models/user");
+const group = require("../../db/models/group");
+
+const router = express.Router();
+
+module.exports = router;
