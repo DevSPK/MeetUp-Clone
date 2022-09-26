1ca369499ae2ea6faadee3075a32c4ba4f4f4942
DevSPK <seankennedy75@gmail.com>
2022-09-26 06:56:51 -0700

    groups page done

--- frontend/src/components/GroupsPage/index.js
+++ frontend/src/components/GroupsPage/index.js
@@ -6,7 +6,8 @@ import {
 	Route,
 	Switch,
 	// NavLink,
-	Link
+	Link,
+	NavLink
 } from "react-router-dom";
 import { SingleGroup } from "../SingleGroup";
 import GroupInput from "../GroupInput";
@@ -34,57 +35,103 @@ const GroupsPage = () => {
 
 	//// console.log(groupsList.map(group => ));
 
-	let content = null;
+	// let content = null;
 
-	if (showCreateGroupForm) {
-		content = (
-			<Route path='/groups/'>
-				<GroupInput
-					hideForm={() => setShowCreateGroupForm(false)}
-				/>
-			</Route>
-		);
-	}
+	// if (showCreateGroupForm) {
+	// 	content = (
+	// 		<Route path=''/start-a-group''>
+	// 			<GroupInput
+	// 				hideForm={() => setShowCreateGroupForm(false)}
+	// 			/>
+	// 		</Route>
+	// 	);
+	// }
 
 	return (
 		<div>
 			<div className='groups-page'>
-				<h1>Groups Page</h1>
-				<ul className='groups-list-item'>
-					{groupsList.map(
-						({
-							id,
-							name,
-							city,
-							state,
-							numMembers,
-							previewImage,
-							about
-						}) => (
+				<div className='groups-events-createGroups-links'>
+					<div className='top-groups-link-container top-links'>
+						<NavLink
+							to='/groups'
+							className='top-groups-link'>
+							Groups
+						</NavLink>
+					</div>
+					<div className='top-events-link-container top-links'>
+						<NavLink
+							to='/events'
+							className='top-events-link'>
+							Events
+						</NavLink>
+					</div>
+					<div className='top-create-groups-link-container top-links'>
+						<NavLink
+							to='/start-a-group'
+							className='top-create-groups-link'>
+							Start a new group
+						</NavLink>
+					</div>
+				</div>
+				<div className='groups-list'>
+					<ul className='groups-list-item'>
+						{groupsList.map((group) => (
 							<Link
-								key={id}
-								to={`/groups/${id}`}>
+								style={{ textDecoration: "none" }}
+								key={group.id}
+								to={`/groups/${group.id}`}>
 								<li>
-									<h1>{name}</h1>
-									<img
-										src={previewImage}
-										alt='a depiction of this group'
-									/>
+									<div className='group-image-container'>
+										<img
+											className='group-image'
+											src={group.previewImage}
+											alt='a depiction of this group'
+										/>
+									</div>
+									<div className='group-text-items'>
+										<div className='group-name-container'>
+											<h1
+												className='group-name'
+												style={{ textDecoration: "none" }}>
+												{group.name}
+											</h1>
+										</div>
+										<div className='group-city-state-container'>
+											<h2
+												style={{ textDecoration: "none" }}
+												className='group-city-state'>{`${group.city}, ${group.state}`}</h2>
+										</div>
+										<div className='group-about-container'>
+											<p
+												style={{ textDecoration: "none" }}
+												className='group-about'>
+												{group.about}
+											</p>
+										</div>
+										<div
+											style={{ textDecoration: "none" }}
+											className='group-members-type-container'>
+											<p className='group-num-members-type'>
+												{`${group.numMembers} members · `}
+												{group.private && "Private"}
+												{!group.private && "Public"}
+											</p>
+										</div>
+									</div>
 								</li>
 							</Link>
-						)
-					)}
-					{/* <li>
+						))}
+						{/* <li>
 					<Link to={"/groups"}>Create a Group</Link>
 				</li> */}
-				</ul>
-				<button
+					</ul>
+				</div>
+				{/* <button
 					onClick={() => setShowCreateGroupForm(true)}>
 					Create New Group
 				</button>
 
-				<Switch></Switch>
-				<div>{content}</div>
+				<div>{content}</div> */}
 			</div>
 		</div>
 	);
