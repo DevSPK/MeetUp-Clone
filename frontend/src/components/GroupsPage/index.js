b6f4d2e630a29089204ded6f14cb29ad3147c40d
DevSPK <seankennedy75@gmail.com>
2022-09-28 02:28:47 -0700

    groups page almost done

--- frontend/src/components/GroupsPage/index.js
+++ frontend/src/components/GroupsPage/index.js
@@ -3,140 +3,127 @@ import { useSelector, useDispatch } from "react-redux";
 import { thunkReadAllGroups } from "../../store/groups";
 import { useEffect, useState } from "react";
 import {
-	Route,
-	Switch,
-	// NavLink,
-	Link,
-	NavLink
+  Route,
+  Switch,
+  // NavLink,
+  Link,
+  NavLink
 } from "react-router-dom";
 import { SingleGroup } from "../SingleGroup";
 import GroupInput from "../GroupInput";
 import "./GroupsPage.css";
 
 const GroupsPage = () => {
-	const [showCreateGroupForm, setShowCreateGroupForm] =
-		useState(false);
+  const [showCreateGroupForm, setShowCreateGroupForm] = useState(false);
 
-	const groupsList = useSelector((state) =>
-		Object.values(state.groups)
-	);
+  const groupsList = useSelector((state) => Object.values(state.groups));
 
-	// console.log("this is groupsList", groupsList);
+  // console.log("this is groupsList", groupsList);
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(thunkReadAllGroups());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(thunkReadAllGroups());
+  }, [dispatch]);
 
-	if (!groupsList) {
-		return null;
-	}
+  if (!groupsList) {
+    return null;
+  }
 
-	//// console.log(groupsList.map(group => ));
+  //// console.log(groupsList.map(group => ));
 
-	// let content = null;
+  // let content = null;
 
-	// if (showCreateGroupForm) {
-	// 	content = (
-	// 		<Route path=''/start-a-group''>
-	// 			<GroupInput
-	// 				hideForm={() => setShowCreateGroupForm(false)}
-	// 			/>
-	// 		</Route>
-	// 	);
-	// }
+  // if (showCreateGroupForm) {
+  // 	content = (
+  // 		<Route path=''/start-a-group''>
+  // 			<GroupInput
+  // 				hideForm={() => setShowCreateGroupForm(false)}
+  // 			/>
+  // 		</Route>
+  // 	);
+  // }
 
-	return (
-		<div>
-			<div className='groups-page'>
-				<div className='groups-events-createGroups-links'>
-					<div className='top-groups-link-container top-links'>
-						<NavLink
-							to='/groups'
-							className='top-groups-link'>
-							Groups
-						</NavLink>
-					</div>
-					<div className='top-events-link-container top-links'>
-						<NavLink
-							to='/events'
-							className='top-events-link'>
-							Events
-						</NavLink>
-					</div>
-					<div className='top-create-groups-link-container top-links'>
-						<NavLink
-							to='/start-a-group'
-							className='top-create-groups-link'>
-							Start a new group
-						</NavLink>
-					</div>
-				</div>
-				<div className='groups-list'>
-					<ul className='groups-list-item'>
-						{groupsList.map((group) => (
-							<Link
-								style={{ textDecoration: "none" }}
-								key={group.id}
-								to={`/groups/${group.id}`}>
-								<li>
-									<div className='group-image-container'>
-										<div>
-											<img
-												className='group-image'
-												src={group.previewImage}
-												alt='a depiction of this group'
-											/>
-										</div>
-									</div>
-									<div className='group-text-items'>
-										<div className='group-name-container'>
-											<h1
-												className='group-name'
-												style={{ textDecoration: "none" }}>
-												{group.name}
-											</h1>
-										</div>
-										<div className='group-city-state-container'>
-											<h2
-												style={{ textDecoration: "none" }}
-												className='group-city-state'>{`${group.city}, ${group.state}`}</h2>
-										</div>
-										<div className='group-about-container'>
-											<p
-												style={{ textDecoration: "none" }}
-												className='group-about'>
-												{group.about}
-											</p>
-										</div>
-										<div
-											style={{ textDecoration: "none" }}
-											className='group-members-type-container'>
-											<p className='group-num-members-type'>
-												{`${group.numMembers} member(s) · `}
-												{group.private && "Private"}
-												{!group.private && "Public"}
-											</p>
-										</div>
-									</div>
-								</li>
-							</Link>
-						))}
-						{/* <li>
+  return (
+    <div className='groups-page-container'>
+      <div className='groups-page-header-container'>
+        <div className='header-groups-link-container  header-links'>
+          <div className='header--container__links header-links'>
+            <NavLink
+              to='/groups'
+              className='header-groups-link'>
+              Groups
+            </NavLink>
+          </div>
+          <div className='header-events-link-container  header-links'>
+            <NavLink
+              to='/events'
+              className='header-events-link'>
+              Events
+            </NavLink>
+          </div>
+          <div className='header-create-groups-link-container  header-links'>
+            <NavLink
+              to='/start-a-group'
+              className='header-create-groups-link'>
+              Start a new group
+            </NavLink>
+          </div>
+        </div>
+      </div>
+
+      <div className='grid--column__middle'>
+        {groupsList.map((group) => (
+          <div
+            style={{ textDecoration: "none" }}
+            key={group.id}
+            to={`/groups/${group.id}`}
+            className='grid-card--container'>
+            <Link to={`/groups/${group.id}`}>
+              <div className='grid-card--text__container'>
+                <img
+                  class='grid-card--image__preview'
+                  src={group.previewImage}
+                  alt='a depiction of this group'
+                />
+                <p
+                  className='grid-card--text__name'
+                  style={{ textDecoration: "none" }}>
+                  {group.name}
+                </p>
+                <p
+                  style={{ textDecoration: "none" }}
+                  className='grid-card--text__city-state  uppercase'>
+                  {`${group.city}, ${group.state}`}
+                </p>
+                <p
+                  style={{ textDecoration: "none" }}
+                  className='grid-card--text__about'>
+                  {group.about}
+                </p>
+                <p
+                  className='grid-card--text__members-type'
+                  style={{ textDecoration: "none" }}>
+                  {`${group.numMembers} member(s) · `}
+                  {group.private && "Private"}
+                  {!group.private && "Public"}
+                </p>
+              </div>
+            </Link>
+          </div>
+        ))}
+        {/* <li>
 					<Link to={"/groups"}>Create a Group</Link>
 				</li> */}
-					</ul>
-				</div>
-				{/* <button
+      </div>
+      {/* <button
 					onClick={() => setShowCreateGroupForm(true)}>
 					Create New Group
 				</button>
 
 				<div>{content}</div> */}
-			</div>
-		</div>
-	);
+    </div>
+  );
 };
 
 export default GroupsPage;
--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -8,128 +8,121 @@ import { Link } from "react-router-dom";
 import LoginFormModal from "../LoginFormModal";
 
 const SplashPage = () => {
-	return (
-		<main className='flexContainer'>
-			<div className='middleVerticalContainer'>
-				<div className='topHorzContainer'>
-					<div className='text-container'>
-						<div className='headline-container'>
-							<h1>
-								Celebrating 200 hours of made up connections
-								on Treffenklon
-							</h1>
-						</div>
-						<div className='paragraph-container'>
-							<p>
-								Whatever you're looking to do this year,
-								Treffenklon can help. For 200 hours, made up
-								folks have turned to Meetup to meet other
-								made up folks, make friends, find support,
-								grow a business, and explore their
-								interests. Thousands of made up events are
-								happening every day—join the fun.
-							</p>
-						</div>
-					</div>
-					<div className='imgContainer'>
-						<img
-							src={videoMeeting}
-							alt='online conference call'
-							id='videoMeetingImg'
-						/>
-					</div>
-				</div>
-			</div>
-			<div className='middle-horz-container'>
-				<div className='middle-text-container'>
-					<div className='middle-headline-container'>
-						<h2>How Treffenklon works</h2>
-					</div>
-					<div className='middle-paragraph-container'>
-						<p>
-							Meet made up people who share your interests
-							through online and in-person events. It's free
-							to create an account.
-						</p>
-					</div>
-				</div>
-				<div className='middle-action-links-container'>
-					<div className='outer-item-container'>
-						<div className='image-container'>
-							<img
-								src={leftImage}
-								alt='high-five cartoon'
-								className='action-link-images'
-							/>
-						</div>
-						<div className='link-container'>
-							<Link
-								className='action-link'
-								to='/groups'>
-								Join a group
-							</Link>
-						</div>
-						<div className='paragraph-container'>
-							Do what you love, meet others who love it,
-							find your community. The rest is history!
-						</div>
-					</div>
-					<div className='outer-item-container'>
-						<div className='image-container'>
-							<img
-								src={middleImage}
-								alt='ticket cartoon'
-								className='action-link-images'
-							/>
-						</div>
-						<div className='link-container'>
-							<Link
-								className='action-link'
-								to='/events'>
-								Find an event
-							</Link>
-						</div>
-						<div className='paragraph-container'>
-							Events are happening on just about any topic
-							you can think of.
-						</div>
-					</div>
-					<div className='outer-item-container'>
-						<div className='image-container'>
-							<img
-								src={rightImage}
-								alt='silhouettes of three cartoon people'
-								className='action-link-images'
-							/>
-						</div>
-						<div className='link-container'>
-							<Link
-								className='action-link'
-								to='/signup'>
-								Start a group
-							</Link>
-						</div>
-						<div className='paragraph-container'>
-							You don't have to be an expert to gather
-							people together and explore shared interests.
-						</div>
-					</div>
-				</div>
-				<div className='middle-bottom-sign-up-button-container'>
-					<div className='bottom-space-container'>
-						<Link to='/signup'>
-							<button className='action-signup-button'>
-								Join Treffenklon
-							</button>
-						</Link>
-					</div>
-				</div>
-			</div>
-			{/* <footer className='bottom-container'>
+  return (
+    <main className='flexContainer'>
+      <div className='middleVerticalContainer'>
+        <div className='topHorzContainer'>
+          <div className='text-container'>
+            <div className='headline-container'>
+              <h1>
+                Celebrating 200 hours of made up connections on Treffenklon
+              </h1>
+            </div>
+            <div className='paragraph-container'>
+              <p className='splash--text'>
+                Whatever you're looking to do this year, Treffenklon can help.
+                For 200 hours, made up folks have turned to Meetup to meet other
+                made up folks, make friends, find support, grow a business, and
+                explore their interests. Thousands of made up events are
+                happening every day—join the fun.
+              </p>
+            </div>
+          </div>
+          <div className='imgContainer'>
+            <img
+              src={videoMeeting}
+              alt='online conference call'
+              id='videoMeetingImg'
+            />
+          </div>
+        </div>
+      </div>
+      <div className='middle-horz-container'>
+        <div className='middle-text-container'>
+          <div className='middle-headline-container'>
+            <h2>How Treffenklon works</h2>
+          </div>
+          <div className='middle-paragraph-container'>
+            <p className='splash--text'>
+              Meet made up people who share your interests through online and
+              in-person events. It's free to create an account.
+            </p>
+          </div>
+        </div>
+        <div className='middle-action-links-container'>
+          <div className='outer-item-container'>
+            <div className='image-container'>
+              <img
+                src={leftImage}
+                alt='high-five cartoon'
+                className='action-link-images'
+              />
+            </div>
+            <div className='link-container'>
+              <Link
+                className='action-link'
+                to='/groups'>
+                Join a group
+              </Link>
+            </div>
+            <div className='paragraph-container'>
+              Do what you love, meet others who love it, find your community.
+              The rest is history!
+            </div>
+          </div>
+          <div className='outer-item-container'>
+            <div className='image-container'>
+              <img
+                src={middleImage}
+                alt='ticket cartoon'
+                className='action-link-images'
+              />
+            </div>
+            <div className='link-container'>
+              <Link
+                className='action-link'
+                to='/events'>
+                Find an event
+              </Link>
+            </div>
+            <div className='paragraph-container'>
+              Events are happening on just about any topic you can think of.
+            </div>
+          </div>
+          <div className='outer-item-container'>
+            <div className='image-container'>
+              <img
+                src={rightImage}
+                alt='silhouettes of three cartoon people'
+                className='action-link-images'
+              />
+            </div>
+            <div className='link-container'>
+              <Link
+                className='action-link'
+                to='/signup'>
+                Start a group
+              </Link>
+            </div>
+            <div className='paragraph-container'>
+              You don't have to be an expert to gather people together and
+              explore shared interests.
+            </div>
+          </div>
+        </div>
+        <div className='middle-bottom-sign-up-button-container'>
+          <div className='bottom-space-container'>
+            <Link to='/signup'>
+              <button className='action-signup-button'>Join Treffenklon</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      {/* <footer className='bottom-container'>
 				<div className='bottom-horz-container'></div>
 			</footer> */}
-		</main>
-	);
+    </main>
+  );
 };
 
 export default SplashPage;
